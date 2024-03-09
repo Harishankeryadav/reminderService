e6fe06d89417d1baba1124ff24527a4783387b7c
Hari Shanker Yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-03-09 15:06:02 +0530

    crated and connected all the servers. and setup of rabbitmq to the project

--- src/index.js
+++ src/index.js
@@ -1,28 +1,56 @@
-const express = require('express')
-const bodyparser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-// const { sendBasicEmail } = require('./services/email-service');
-const cron = require('node-cron');
-const setUpAndStartServer = () => {
+
+const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
+
+const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
     const app = express();
-    app.use(bodyparser.json());
-    app.use(bodyparser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
     app.listen(PORT, () => {
-        console.log(`server started on port ${PORT}`);
-
+        console.log(`Server started at port ${PORT}`);
+        // jobs();
+        
         // sendBasicEmail(
         //     'support@gmail.com',
         //     'harishankeryadav927@gmail.com',
         //     'testing',
         //     'hello world'
         // )
-
-        cron.schedule('*/2 * * * *', () => {
-            console.log('running after every two minutes');
-        });
+        
+        // cron.schedule('*/2 * * * *', () => {
+        //     console.log('running after every two minutes');
+        // });
     });
 }
 
-setUpAndStartServer();
+setupAndStartServer();
+
+
+
+
+
+
+
+
+
+// create and tickets service in postman with this and more example
+// {
+//     "subject":"this is ticket 1",
+//     "content":"this is some content to send",
+//     "recepientEmail":"harishankeryadav927@gmail.com",
+//     "notificationTime":"2024-03-07T04:01:00.000"
+//     }
